584efad314500b33e980ba8e90c1c71ee834e584
Rodrigo Cruz <rodrigoalejandro.cruzfagiani@gmail.com>
2026-09-15 18:16:12 -0600

    commit again

---
+++ server/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+// Importación de rutas
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  }),
+);
+
+// Conectar las rutas
+
+export { app };
