10949e51b3ab2ba4178f3c576955172b1caeec9f
Rodrigo Cruz <rodrigoalejandro.cruzfagiani@gmail.com>
2026-09-18 15:39:54 -0600

    feat(srv): added testing dependencies.

---
+++ server/src/test/setup.ts
@@ -0,0 +1,36 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import request from 'supertest';
+
+import { app } from '../app';
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'testkey';
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+
+  if (mongoose.connection.db) {
+    const collections = await mongoose.connection.db.collections();
+
+    for (let collection of collections) {
+      await collection.deleteMany({});
+    }
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
