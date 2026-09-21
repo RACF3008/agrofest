51ae62faa02b9eb57a3feda5866efc008a768644
Rodrigo Cruz <rodrigoalejandro.cruzfagiani@gmail.com>
2026-09-20 21:09:44 -0600

    forzar pull-request

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ const start = async () => {
   console.log("Starting up...");
 
   if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI must be defined");
+    throw new Error("MONGO_URI must be defined!");
   }
 
   await mongoose.connect(process.env.MONGO_URI);
