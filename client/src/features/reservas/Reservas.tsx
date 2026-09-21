ce3fd9a0584a76f5bdd90b69181cf07e1dc1d0f7
Rodrigo Cruz <rodrigoalejandro.cruzfagiani@gmail.com>
2026-09-20 21:25:42 -0600

    adicion de comentarios

--- client/src/features/reservas/Reservas.tsx
+++ client/src/features/reservas/Reservas.tsx
@@ -27,6 +27,7 @@ const Reservas = () => {
 
   return (
     <div className="relative flex-1 p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 items-start">
+      {/* LISTA DE RESERVAS */}
       {reservas.length === 0 ? (
         <p className="absolute text-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-2xl font-semibold text-gray-300">
           No tienes reservas
@@ -41,6 +42,7 @@ const Reservas = () => {
         ))
       )}
 
+      {/* BOTON NUEVA RESERVA */}
       <button
         onClick={() => navigate("/reservas/crear")}
         className="fixed right-4 bottom-14 size-16 bg-accent rounded-full text-4xl font-bold hover:cursor-pointer flex justify-center items-center"
